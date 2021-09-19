39f9fa58757993f0b9a077c7caf5578182a918af
Nahuel Gómez <ngomezpf@gmail.com>
2021-09-19 01:22:42 -0300

    Versión final

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
+// DOM
 const containerImage = document.querySelector(".container__image");
-const boton = document.querySelector('#boton');
-
-// VARIABLES
-let valor = Math.random() * 7;
-let valorParseado = parseInt(valor);
+const boton = document.querySelector("#boton");
+let isWorking = false;
 
 // ARRAY CON MAPAS
-const mapas = ["images/ascent.jpg", "images/bind.jpg", "images/breeze.jpg", "images/fracture.jpg", "images/haven.jpg", "images/icebox.jpg", "images/split.jpg",];
+const mapas = [
+  "images/ascent.jpg",
+  "images/bind.jpg",
+  "images/breeze.jpg",
+  "images/fracture.jpg",
+  "images/haven.jpg",
+  "images/icebox.jpg",
+  "images/split.jpg",
+];
 
-// Div con las imágenes de los mapas
-const mapaSorteado = document.createElement("img");
-mapaSorteado.classList.add("mapaSorteado");
-mapaSorteado.src = mapas[valorParseado];
+const HandlerRandomMap = () => {
+  // VARIABLES
+  let valor = Math.random() * 7;
+  let valorParseado = parseInt(valor);
 
-containerImage.appendChild(mapaSorteado); //NO BORRAR
+  // Div con las imágenes de los mapas
+  const mapaSorteado = document.createElement("img");
+  mapaSorteado.classList.add("mapaSorteado");
+  mapaSorteado.src = mapas[valorParseado];
+  containerImage.innerHTML = "";
+  containerImage.appendChild(mapaSorteado);
+};
 
+const HandlerGenerateMap = () => {
+  if (!isWorking) {
+    isWorking = true;
+    const intervaloUno = setInterval(() => {
+      HandlerRandomMap();
+    }, 150);
+    setTimeout(() => {
+      clearInterval(intervaloUno);
+      isWorking = false;
+    }, 1000 * 3);
+  }
+};
 
-
-// console.log(mapaSorteado);
+boton.addEventListener("click", HandlerGenerateMap);
